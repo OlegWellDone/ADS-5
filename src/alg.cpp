2bf7ff08850d7293026643dfa58915ee68c3d9ad
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2022-04-02 21:18:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,12 +4,14 @@
 #include "tstack.h"
 
 int prior(char t) {
-switch(t) {
-case '(' : return 0;
-case ')' : return 1;
-case '+', '-' : return 2;
-case '*', '/' : return 3;
-default : return 8;
+switch (t) {
+  case '(' : return 0;
+  case ')' : return 1;
+  case '+' : return 2;
+  case '-' : return 2;
+  case '*' : return 3;
+  case '/' : return 3;
+  default : return 8;
 }
 }
 
@@ -17,7 +19,7 @@ std::string infx2pstfx(std::string inf) {
   TStack <char, 50> zzz;
 int pr;
 std :: string itog;
-for (int i  = 0, i <= inf.size(); i++) {
+for (int i  = 0; i <= inf.size(); i++) {
   pr = prior(inf[i]);
 switch  (pr) {
   case 0 : zzz.push(inf[i]); break;
@@ -32,26 +34,28 @@ break;
   case 2 : {
 if ((prior(zzz.get()) <= 2) || zzz.isEmpty()) {
 zzz.push(inf[i]);
+  break;
 } else {
 while (prior(zzz.get()) > 2) {
 itog.push_back(zzz.get());
 itog.push_back(' ');
 zzz.pop();
 }
-zzz.push(inf[I]);
+zzz.push(inf[i]);
 break;
 }
+  }
   case 3 : {
 if ((prior(zzz.get()) <= 3) || zzz.isEmpty()) {
 zzz.push(inf[i]);
 }
 break;
-}
+  }
   case 8 : {
 itog.push_back(inf[i]);
 itog.push_back(' ');
 break;
-}
+  }
 }
 }
 while (!zzz.isEmpty()) {
@@ -67,9 +71,9 @@ TStack <int, 50> zzz;
 int itog;
 for (int i = 0; i <= pref.size(); i++) {
 if ((pref[i] != ' ') && (prior(pref[i] == 8)) {
-zzz.push((int)pref[i]);
+zzz.push(<int>pref[i]);
 } else {
-  switch(pref[i]) {
+  switch (pref[i]) {
     case '+' : {
       itog = zzz.get();
       zzz.pop();
