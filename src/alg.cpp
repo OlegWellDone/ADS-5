15135026d8f8b30e799366af3750881efaa9b524
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2022-04-03 00:48:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -79,43 +79,43 @@ return itog;
 }
 
 int eval(std::string pref) {
-TStack <int, 50> zzz;
+TStack <int, 50> zzzz;
 int itog, temp;
 for (int i = 0; i < pref.size(); i++) {
 if ((static_cast<int>(pref[i]) > 0) && (static_cast<int>(pref[i]) < 10)) {
-zzz.push(static_cast<int>(pref[i]));
+zzzz.push(static_cast<int>(pref[i]));
 } else if (prior(pref[i]) > 1) {
   switch (pref[i]) {
     case '+' : {
-      temp = zzz.get();
-      zzz.pop();
-      itog = zzz.get();
-      zzz.pop();
-      zzz.push(itog + temp);
+      temp = zzzz.get();
+      zzzz.pop();
+      itog = zzzz.get();
+      zzzz.pop();
+      zzzz.push(itog + temp);
       break;
     }
     case '-' : {
-      temp = zzz.get();
-      zzz.pop();
-      itog = zzz.get();
-      zzz.pop();
-      zzz.push(itog - temp);
+      temp = zzzz.get();
+      zzzz.pop();
+      itog = zzzz.get();
+      zzzz.pop();
+      zzzz.push(itog - temp);
       break;
     }
     case '*' : {
-      temp = zzz.get();
-      zzz.pop();
-      itog = zzz.get();
-      zzz.pop();
-      zzz.push(itog * temp);
+      temp = zzzz.get();
+      zzzz.pop();
+      itog = zzzz.get();
+      zzzz.pop();
+      zzzz.push(itog * temp);
       break;
     }
     case '/' : {
-      temp = zzz.get();
-      zzz.pop();
-      itog = zzz.get();
-      zzz.pop();
-      zzz.push(itog / temp);
+      temp = zzzz.get();
+      zzzz.pop();
+      itog = zzzz.get();
+      zzzz.pop();
+      zzzz.push(itog / temp);
       break;
     }
   }
