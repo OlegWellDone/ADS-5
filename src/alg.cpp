33105d204f040451e9de9ae2b97bdec79f61270b
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2022-04-02 14:15:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -17,23 +17,23 @@ int ischar(char t) {
 
 std::string infx2pstfx(std::string inf) {
   TStack<char, 100> stackznakov;
-  char chto = '', razd = ' ';
+  char chto = ' ', razd = ' ';
   std::string itog = " ";
   for (int i = 0; i <= inf.length(); i++) {
     chto = inf[i];
     if (ischar(chto) == 4) {
-      itog += chto;
-      itog += razd;
+      itog.concat(chto);
+      itog.concat(razd);
       }
       if (ischar(chto) == 1) {
-      while (stackznakov.top() != 0) {
-        itog += stackznakov.pop();
-        itog += razd;
+      while (ischar(stackznakov.top()) != 0) {
+        itog.concat(stackznakov.pop());
+        itog.concat(razd);
         }
       } else if ((ischar(chto) >= ischar(stackznakov.top())) &&
                  (ischar(chto) != 0) && (ischar(chto) != 1)) {
-        itog += chto;
-        itog += razd;
+        itog.concat(chto);
+        itog.concat(razd);
       } else {
         stackznakov.push(chto);
       }
@@ -45,7 +45,7 @@ int eval(std::string pref) {
   TStack<int, 100> stackzifr;
   char chto = ' ';
   std::string uu = " ";
-  uint64_t resh = 0, kk = 0;
+  int resh = 0, kk = 0;
   for (int i = 0; i <= pref.length(); i++) {
     chto = pref[i];
     if (ischar(chto) == 4) {
